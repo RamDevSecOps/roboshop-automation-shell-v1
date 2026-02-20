9b8d5aeddcc51a89cacd73fd70fc140d61be7116
Rama Murthy koppulla <ramamurthykoppulla9@gmail.com>
2026-02-21 03:17:30 +0530

    updated shipping

--- shipping.sh
+++ shipping.sh
@@ -14,9 +14,9 @@ mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
 dnf install mysql -y
-mysql -h mysql-dev.ramdevsecops -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.ramdevsecops -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.ramdevsecops -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.ramdevsecops.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.ramdevsecops.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql-dev.ramdevsecops.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl daemon-reload
 systemctl enable shipping
